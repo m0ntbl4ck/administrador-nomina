c4d2cc1ef86aaa6ff77be30a5c646d2bf650837f
natalyq1 <onatylili@gmail.com>
2023-05-10 22:29:52 -0500

    Se cambio los estilos de agregar y listar empleados

--- cliente/administrador/js/listado-empleados.js
+++ cliente/administrador/js/listado-empleados.js
@@ -15,18 +15,19 @@ $(document).ready(function () {
         console.log(respuesta[i]);
   $('#tabla-listado-empleados').append(
       `<tr>
-      <td class="text-center">${respuesta[i].dni}</td>
-      <td class="text-center">${respuesta[i].nombre}</td>
-      <td class="text-center">${respuesta[i].apellido}</td>
-      <td class="text-center">${respuesta[i].fecha_nacimiento}</td>
-      <td class="text-center">${respuesta[i].telefono}</td>
-      <td class="text-center">${respuesta[i].email}</td>
-      <td class="text-center">${respuesta[i].direccion}</td>
-      <td class="text-center">${respuesta[i].cargo}</td>
-      <td  class="text-center">
+      <td>${respuesta[i].dni}</td>
+      <td>${respuesta[i].nombre}</td>
+      <td>${respuesta[i].apellido}</td>
+      <td>${respuesta[i].fecha_nacimiento}</td>
+      <td>${respuesta[i].telefono}</td>
+      <td>${respuesta[i].email}</td>
+      <td>${respuesta[i].direccion}</td>
+      <td>${respuesta[i].cargo}</td>
+      <td >
       <button 
-      class="btn btn-danger mx-3"
-      onclick="eliminar_empleado('${respuesta[i]._id}')">Eliminar</button><td>
+      class="btn btn-danger"
+      onclick="eliminar_empleado('${respuesta[i]._id}')">Eliminar</button>
+      </td>
       </tr>`
     )
   }     
